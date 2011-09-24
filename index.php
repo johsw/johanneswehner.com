2797c98ceb8a496056b4a4569b357fe78614e7ad
Johannes Wehner <johsw@information.dk>
2011-09-24 15:48:38 +0200

    Current site.

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
+    "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" version="XHTML+RDFa 1.0" xml:lang="en" xmlns:foaf="http://xmlns.com/foaf/0.1/">
+  <head>
+    <title>
+      Johannes Wehner // Tagging, Autotagging, Open Source Development
+    </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Business website for Johannes Wehner, tagging and open source web consultant. I do Drupal!" />
+    <meta name="keywords" content="Tagger, Drupal, PHP, MySQL, jQUery, Open Source, Web, Semantic Web, Web development" />
+    <link href="style.css" rel="stylesheet" media="screen, projection" type="text/css" />
+  </head>
+  <body>
+    <div about="#me" typeof="foaf:Person" id="main">
+      <div id="intro-wrapper">
+        <div id="intro">
+          <h1>
+            Hi! My name is <span id="my-name" property="foaf:name">Johannes Wehner</span>
+          </h1>
+          <h2>
+            I do open source web development and consulting. And tagging.
+          </h2>
+        </div>
+      </div>
+      <div id="contents-wrapper">
+        <div id="contents">
+          <div id="tools" class="single-col">
+            <h3>
+              I use tools like
+            </h3>
+            <p>
+              <a href="http://drupal.org/">Drupal</a>, <a href="http://php.net">PHP</a>, <a href="http://jquery.com/">jQuery</a>, <a href="http://httpd.apache.org/">Apache</a>, <a href="http://mysql.com">MySQL</a>, <a href="http://varnish-cache.org/">Varnish</a>, <a href="http://www.sphinxsearch.com/">Sphinx</a>, <a href="http://memcached.org/">Memcached</a> and <a href="http://www.centos.org/">CentOS Linux.</a>
+            </p>
+            <br />
+            <p>...and the tagging project I founded: <a href="http://tagger.dk">Tagger</a></p>
+          </div>
+          <div id="experience" class="single-col">
+            <h3>
+              I've helped
+            </h3>
+            <p>
+              <a href="http://www.information.dk/">Dagbladet Information</a>, <a href="http://tv2.dk">Tv2</a>, <a href="http://borsen.dk">Børsen</a>, <a href="http://www.al-akhbar.com">Al-Akhbar</a>, <a href="http://www.ms.dk">Mellemfolkeligt Samvirke</a>, <a href="http://aoh.dk">aoh.dk</a>, <a href="http://uovo.dk">Uovo</a>, <a href="http://kommunikationsfirmaet.com/">Kommunikationsfirmaet</a>
+            </p>
+          </div>
+          <div id="tasks" class="single-col">
+            <h3>
+              I do
+            </h3>
+            <p>
+              Content metadata consultancy, Tagger implementation and integration, Web Application Development, Migration and Optimization.
+            </p>
+          </div>
+          <div id="contact" class="single-col last">
+            <h3>
+              Contact
+            </h3>
+            <p>
+              <a href="http://40c.dk">40C.dk</a><br />
+              <a rel="foaf:mbox" href="mailto:mail@johanneswehner.com">mail@johanneswehner.com</a><br />
+              <a href="http://dk.linkedin.com/in/johanneswehner">LinkedIn</a><br />
+              <a href="http://twitter.com/johsw">twitter.com/johsw</a><br />
+            </p>
+          </div>
+        </div>
+      </div>
+      <div id="footer-wrapper">
+        <div id="footer">
+          <p>
+            Copenhagen, Denmark - CVR: 32749534
+          </p>
+        </div>
+      </div>
+    </div><script type="text/javascript">
+//<![CDATA[
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-3277919-4']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    //]]>
+    </script>
+  </body>
+</html>
