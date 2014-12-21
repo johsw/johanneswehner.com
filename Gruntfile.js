8e4b6e4172966ba1c111e14ef7be3cf38657eba4
Johannes Wehner <johsw@information.dk>
2014-12-21 23:20:40 +0100

    Tons. Responsive.

--- Gruntfile.js
+++ Gruntfile.js
@@ -322,7 +322,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'fonts/*'
           ]
         }, {
           src: 'node_modules/apache-server-configs/dist/.htaccess',
