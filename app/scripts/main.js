df72fdc06a26501541256d630a61a3628f2c7761
Johannes Wehner <mail@johanneswehner.com>
2016-11-05 22:05:29 +0100

    Removing modernizer + jquery.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,34 @@
-$(document).ready(function() {
-  $welcome = $('#welcome');
+var welcomeElement;
+var welcomeShown;
+var margin = window.innerHeight * 0.10;
+
+var init = function() {
+  welcomeElement = document.getElementById('welcome');
   welcomeShown = true;
-  $( window ).scroll(function(e) {
-    checkPosition();
-  });
-  checkPosition = function() {
-    margin = ($(window).height()/3) * 2;
-    if (($welcome.offset().top - $(window).scrollTop() - margin) > 0) {
-      if (!welcomeShown) {
-        $('body').removeClass('welcome-hidden');
-        welcomeShown = true;
-      }
-    }
-    else {
-      if (welcomeShown) {
-        $('body').addClass('welcome-hidden');
-        welcomeShown = false;
-      }
+  window.onscroll = checkPosition;
+}
+
+var checkPosition = function() {
+
+  var rectangle = welcomeElement.getBoundingClientRect();
+  var welcomeOffset = rectangle.top;
+
+  var scrollPos = document.body.scrollTop;
+
+  if ((welcomeOffset - scrollPos - margin) > 0) {
+
+    if (!welcomeShown) {
+      document.body.classList.remove('welcome-hidden')
+      welcomeShown = true;
     }
   }
-});
+  else {
+    if (welcomeShown) {
+      document.body.classList.add('welcome-hidden')
+      welcomeShown = false;
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', init());
+
