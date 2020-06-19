4cef002ac2d6b6fe62fd359a5aa505ebbb7b401e
Johannes Wehner <mail@johanneswehner.com>
2020-06-19 22:00:42 +0200

    First draft of the new version

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 "use strict";
 
-//import Canvas from "./canvas";
+import Canvas from "./canvas";
 import Pointer from "./pointer";
 
 var init = function() {
   const pointer = new Pointer();
-  const canvasEl = document.getElementById("canvas");
-  //const canvas = new Canvas(canvasEl);
+  //const canvasEl = document.getElementById("canvas");
+  const canvas = new Canvas();
 };
 
 document.addEventListener("DOMContentLoaded", init());
